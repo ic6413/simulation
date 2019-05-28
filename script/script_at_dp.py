32d32390ac06e041c7b57b8f9bc104b92a718f57
ic6413 <ic6413@gmail.com>
2019-05-28 13:21:42 -0700

    fix again

--- script/script_at_dp.py
+++ script/script_at_dp.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 import getattribute.create_attributes as gc
+import createdata.datatofile as cd
+import calculate.checkoutputfile as cco
+import plotfigure.plottofile as ppf
+
+# attribute
 gc.define_attribute_dict()
 
-import createdata.datatofile as cd
+# thermo
 cd.thermo_hdf5_csv()
+# thermo plot
+variable_name_list = 'all'
+ppf.plotclass(0, 600000).plotthermo(variable_name_list)
+
+# custom
 cd.dump_custom()
 id_i = 15556
 step1 = 57860
 step2 = 57900
 
-import calculate.checkoutputfile as cco
+
 cco.checkoverlap(id_i, step1, step2).checkprint()
 error_tolerence = 1e-7
 method_list = list(range(0, 3))
@@ -17,11 +27,10 @@ cco.checkforce(id_i, step1, step2, error_tolerence, method_list).checkprint()
 cco.check_ft_1j_contact(id_i, step1, step2, error_tolerence, method_list).checkprint()
 cco.check_ft_1w_contact(id_i, step1, step2, error_tolerence, method_list).checkprint()
 
-import plotfigure.plottofile as ppf
+
 ppf.plotclass(step1, step2).plotij(id_i)
 
-variable_name_list = 'all'
-ppf.plotclass(step1, step2).plotthermo(variable_name_list)
+
 
 
 id_i = 15556
