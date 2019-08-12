7267dd46b5a3d77bc9061d268245c4fe2bb687e8
ic6413 <ic6413@gmail.com>
2019-08-12 16:06:12 -0700

    displace atom

--- bashscript/bashcpu.sh
+++ bashscript/bashcpu.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #Submit this script with: sbatch thefilename
 
+# test job
+##SBATCH --qos=debug
+
 # asking for resources
 #SBATCH --time=00:20:00   # walltime
 #SBATCH --mem=20000M               # memory (per node)
-#SBATCH --ntasks=32
+#SBATCH --ntasks=6
 ##SBATCH --nodes=1
 ##SBATCH -B 2:16  # this also bound task to socket/core/thread since the slurm conf with affinity
-##SBATCH --ntasks-per-node=32 #for distributed memory mpi
+##SBATCH --ntasks-per-node=28 #for distributed memory mpi
 
 # ncpus per MPI task. choose ncpus processors per allocated CPU. (only use one)
 ##SBATCH --cpus-per-task=1  #for shared memory openmp
 
-# test job
-#SBATCH --qos=debug
-
 # job info
-#SBATCH -J "H10_test1"   # job name
+#SBATCH -J "H6"   # job name
 #SBATCH --mail-user=hllin@caltech.edu   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
