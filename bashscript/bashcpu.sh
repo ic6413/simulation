041d199da451cf890f524860b5952bf4795dc4f5
ic6413 <ic6413@gmail.com>
2019-08-15 17:00:11 -0700

    nothing

--- bashscript/bashcpu.sh
+++ bashscript/bashcpu.sh
@@ -2,12 +2,12 @@
 #Submit this script with: sbatch thefilename
 
 # test job
-##SBATCH --qos=debug
+#SBATCH --qos=debug
 
 # asking for resources
-#SBATCH --time=00:20:00   # walltime
+#SBATCH --time=00:30:00   # walltime
 #SBATCH --mem=20000M               # memory (per node)
-#SBATCH --ntasks=6
+#SBATCH --ntasks=48
 ##SBATCH --nodes=1
 ##SBATCH -B 2:16  # this also bound task to socket/core/thread since the slurm conf with affinity
 ##SBATCH --ntasks-per-node=28 #for distributed memory mpi
