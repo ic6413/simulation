b000a56c453fd5cd262a98a6ecf0cc6a4412d9af
ic6413 <ic6413@gmail.com>
2019-08-21 13:53:27 -0700

    nothing

--- script/script_at_dp1.py
+++ script/script_at_dp1.py
@@ -4,4 +4,5 @@ import datapath as dp
 import createdata.datatofile as cd
 import calculate.checkoutputfile as cco
 import plotfigure.plotchunk as pc
-pc.plotchunk(210000, "/home/ic6413/lmp_run/test/Shear/Shear_H30/f_2e5_rotate_y/output/fix/file_avspatial_200000")
+for i in range(20000,100,20200):
+    pc.plotchunk(i, dp.lammps_directory + "output/velocity_field/fix.velocity_field.all")
