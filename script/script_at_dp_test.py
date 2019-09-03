c4e21a983eb8df883a25b704719ab8cd741ee709
ic6413 <ic6413@gmail.com>
2019-09-03 13:08:21 -0700

    nothing

--- script/script_at_dp_test.py
+++ script/script_at_dp_test.py
@@ -8,19 +8,19 @@ import plotfigure.plottofile as ppf
 
 # thermo
 cd.thermo_hdf5_csv()
-traceorall = 'all'
+traceorall = 'trace'
 cd.dumptofile(fromtraceorall=traceorall).dump_custom()
 
-"""
-step1 = 0 #23730
-step2 = 3000
-error_tolerence = 1e-15
-method_list = [2,0] # method 2 has smaller error than method 0, 1
 
-for id_i in range(1,4):
+step1 = 0 #23730
+step2 = 20000
+error_tolerence = 1e-15
+method_list = [2] # method 2 has smaller error than method 0, 1
+
+for id_i in range(131689, 131690):
     cco.checkoverlap(id_i, step1, step2).checkprint()
     cco.checkforce(id_i, step1, step2, error_tolerence, method_list).checkprint()
-"""
+
 """
 cd.dumptofile(traceorall).dump_custom_max("c_KEt_atom")
 cd.dumptofile(traceorall).dump_custom_max("z")
