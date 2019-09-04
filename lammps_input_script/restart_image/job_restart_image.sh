32adb4f6f358d926f4214c2f71180eadbc3c9b12
ic6413 <ic6413@gmail.com>
2019-09-04 16:27:03 -0700

    image restart step

--- lammps_input_script/restart_image/job_restart_image.sh
+++ lammps_input_script/restart_image/job_restart_image.sh
@@ -2,19 +2,19 @@
 START=0
 STEPDIFF=1
 END=2
-allrstimage=0
+allrstimage=1
 
 if [ "${allrstimage}" == 1 ]
 then
     FILES=./output/rst/*
     for f in ${FILES}
     do
-        lmp -in ${HOME}/simulation/lammps_input_script/restart_image/in.lmpscript_image_restart -var restartfile ${f}
+        lmp -in ${HOME}/simulation/lammps_input_script/restart_image/in.lmpscript_image_restart -var restartfile ${f} -log log.restartimage
     done
  else
     for step in $(eval echo "{$START..$END..$STEPDIFF}")
     do
-        lmp -in ${HOME}/simulation/lammps_input_script/restart_image/in.lmpscript_image_restart -var restartfile ./output/rst/restart.mpiio.${step}
+        lmp -in ${HOME}/simulation/lammps_input_script/restart_image/in.lmpscript_image_restart -var restartfile ./output/rst/restart.mpiio.${step} log.restartimage
     done   
 fi
 
