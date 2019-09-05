a1fb64b8cd3dd22d73b4f09e01bc490c855742f9
ic6413 <ic6413@gmail.com>
2019-09-05 03:44:50 -0700

    nothing

--- plotfigure/plotchunk.py
+++ plotfigure/plotchunk.py
@@ -15,26 +15,37 @@ from mpl_toolkits.mplot3d import Axes3D
 # import module
 import rename_variable as rv
 import datapath as dp
+import read_setting.read_setting as rr
 # define function for extract data from fix txt to dataframe
 
 velocity_scale = dp.omega_in*dp.r_in
 
-width_dpunit = 17 #(dp.r_out - dp.r_in)/dp.dp0
-height_dpunit = 37
+width_dpunit = int(rr.logfile['ro_dp_unit'])-int(rr.logfile['ri_dp_unit'])
+height_dpunit = float(rr.logfile['zhi_chunk_dp_unit'])
+diameter = float(rr.logfile['dp'])
 
 step_0 = 10000
 d_step = 10000
 
 n_r = dp.N_bin_r
 n_z = dp.N_bin_z
+x_array, y_array = np.meshgrid(
+                               int(rr.logfile['ri_dp_unit']) + (np.arange(n_r)+0.5)/n_r*width_dpunit,
+                               (np.arange(n_z)+0.5)/n_z*height_dpunit,
+                               )
+dx = 1/n_r*width_dpunit
+dy = 1/n_z*height_dpunit
+x_array = x_array.reshape((-1))
+y_array = y_array.reshape((-1))
 
+vol_in_chunks = np.pi*((x_array+0.5*dx)**2-(x_array-0.5*dx)**2)*(y_array+0.5*dy-(y_array-0.5*dy))*diameter**3
 
 
 def plotchunk(step, file):
     
     n_line_0 = (step - step_0)/d_step*(n_r*n_z+1) + 4
     n_line_1 = n_line_0 + n_r*n_z
-    x_array, y_array = np.meshgrid(np.arange(n_r)/n_r*width_dpunit, np.arange(n_z)/n_z*height_dpunit)
+    
 
     with open(file) as f:
         
@@ -83,19 +94,19 @@ def plotchunk(step, file):
     plt.close('all')
 
 
-    quiver_scale = 0.1
-    label_scale = 0.2
+    quiver_scale = 0.2
+    label_scale = 1
     vx_array = 0*df['c_m1'].values
-    vy_array = df['c_m1'].values
+    vy_array = df['c_m1'].values/float(rr.logfile['den'])/vol_in_chunks
     fig1, ax1 = plt.subplots()
     plt.xlabel('r')
     plt.ylabel('z')
     #ax1.set_title('velocity field r-z direction (average over theta)')
-    Q = ax1.quiver(x_array, y_array, vx_array/velocity_scale, vy_array/velocity_scale,
+    Q = ax1.quiver(x_array, y_array, vx_array, vy_array,
                    units='width',angles='xy', scale_units='xy', scale=quiver_scale,
                    )
 
-    ax1.quiverkey(Q, 0.2, 0.9, label_scale, label = 'density', labelpos='E',
+    ax1.quiverkey(Q, 0.2, 0.9, label_scale, label = 'solid fraction', labelpos='E',
                    coordinates='figure', angle=90)
 
     fig1.savefig(dp.f_momentum_mass_field_density_path + str(step))
