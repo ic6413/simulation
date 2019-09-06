eace67258275a2644ec8c3c0bd3ee3f886ce77c1
ic6413 <ic6413@gmail.com>
2019-09-05 19:06:40 -0700

    nothing

--- plotfigure/plotchunk.py
+++ plotfigure/plotchunk.py
@@ -18,14 +18,16 @@ import datapath as dp
 import read_setting.read_setting as rr
 # define function for extract data from fix txt to dataframe
 
-velocity_scale = dp.omega_in*dp.r_in
-
-width_dpunit = int(rr.logfile['ro_dp_unit'])-int(rr.logfile['ri_dp_unit'])
-height_dpunit = float(rr.logfile['zhi_chunk_dp_unit'])
 diameter = float(rr.logfile['dp'])
+width_dpunit = int(rr.logfile['ro_dp_unit'])-int(rr.logfile['ri_dp_unit'])
+ri = diameter*int(rr.logfile['ri_dp_unit'])
+g = float(rr.logfile['g'])
+velocity_scale = dp.omega_in*dp.r_in
+if velocity_scale == 0:
+    Sa_fake = 0.000002
+    velocity_scale = ri*(Sa_fake*g*(width_dpunit)**3*diameter/ri**2)**0.5
 
-step_0 = 10000
-d_step = 10000
+height_dpunit = float(rr.logfile['zhi_chunk_dp_unit'])
 
 n_r = dp.N_bin_r
 n_z = dp.N_bin_z
@@ -41,7 +43,7 @@ y_array = y_array.reshape((-1))
 vol_in_chunks = np.pi*((x_array+0.5*dx)**2-(x_array-0.5*dx)**2)*(y_array+0.5*dy-(y_array-0.5*dy))*diameter**3
 
 
-def plotchunk(step, file):
+def plotchunk(step, step_0, d_step, file):
     
     n_line_0 = (step - step_0)/d_step*(n_r*n_z+1) + 4
     n_line_1 = n_line_0 + n_r*n_z
@@ -57,47 +59,15 @@ def plotchunk(step, file):
         df = pd.DataFrame(data = data, columns = header, dtype = 'float64')
         ## repeat timestep
 
-    quiver_scale = 10
-    label_scale = 10
-    vx_array = np.divide(df['v_mvr'].values, df['c_m1'].values)
-    vy_array = np.divide(df['v_mvz'].values, df['c_m1'].values)
-    fig1, ax1 = plt.subplots()
-    plt.xlabel('r')
-    plt.ylabel('z')
-    #ax1.set_title('velocity field r-z direction (average over theta)')
-    Q = ax1.quiver(x_array, y_array, vx_array/velocity_scale, vy_array/velocity_scale,
-                   units='width',angles='xy', scale_units='xy', scale=quiver_scale,
-                   )
-
-    ax1.quiverkey(Q, 0.2, 0.9, label_scale, label = str(label_scale)+'*arrow length of wall velocity in 45 degree, velocity field r-z direction', labelpos='E',
-                   coordinates='figure', angle=45)
-
-    fig1.savefig(dp.f_momentum_mass_field_rz_path + str(step))
-    plt.close('all')
-
-    quiver_scale = 100
-    label_scale = 100
-
-    vx_array = np.divide(df['v_mvt'].values, df['c_m1'].values)
-    vy_array = np.divide(df['v_mvz'].values, df['c_m1'].values)
-    fig1, ax1 = plt.subplots()
-    plt.xlabel('r(position), theta(velocity)')
-    plt.ylabel('z')
-    #ax1.set_title('velocity field r-z direction (average over theta)')
-    Q = ax1.quiver(x_array, y_array, vx_array/velocity_scale, vy_array/velocity_scale,
-                   units='width',angles='xy', scale_units='xy', scale=quiver_scale,
-                   )
-    ax1.quiverkey(Q, 0.2, 0.9, label_scale, label = str(label_scale)+'*arrow length of wall velocity in 45 degree, velocity field r-theta direction', labelpos='E',
-                   coordinates='figure', angle=45)
-
-    fig1.savefig(dp.f_momentum_mass_field_rtheta_path + str(step))
-    plt.close('all')
-
-
-    quiver_scale = 0.2
-    label_scale = 1
-    vx_array = 0*df['c_m1'].values
-    vy_array = df['c_m1'].values/float(rr.logfile['den'])/vol_in_chunks
+    def divide_zero(a,b):
+        c = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
+        return c
+    vx_array = divide_zero(df['v_mvr'].values,df['c_m1'].values)/velocity_scale
+    vy_array = divide_zero(df['v_mvz'].values,df['c_m1'].values)/velocity_scale
+    v_length_array = (vx_array**2+vy_array**2)**0.5
+    max_v_length = np.amax(v_length_array)
+    quiver_scale = max_v_length/2
+    label_scale = max_v_length/2
     fig1, ax1 = plt.subplots()
     plt.xlabel('r')
     plt.ylabel('z')
@@ -106,8 +76,52 @@ def plotchunk(step, file):
                    units='width',angles='xy', scale_units='xy', scale=quiver_scale,
                    )
 
-    ax1.quiverkey(Q, 0.2, 0.9, label_scale, label = 'solid fraction', labelpos='E',
-                   coordinates='figure', angle=90)
+    ax1.quiverkey(Q, 0.2, 0.9, label_scale,
+                  label = "labescale" + str(label_scale) + "quiver_scale" + str(quiver_scale) + '*arrow length of wall velocity in 45 degree, velocity field r-z direction',
+                  labelpos='E', coordinates='figure', angle=45)
+
+    fig1.savefig(dp.f_momentum_mass_field_rz_path + str(step))
+    plt.close('all')
+
+    vx_array = divide_zero(df['v_mvt'].values,df['c_m1'].values)/velocity_scale
+    vy_array = divide_zero(df['v_mvz'].values,df['c_m1'].values)/velocity_scale
+    v_length_array = (vx_array**2+vy_array**2)**0.5
+    max_v_length = np.amax(v_length_array)
+    quiver_scale = max_v_length/2
+    label_scale = max_v_length/2
+    fig1, ax1 = plt.subplots()
+    plt.xlabel('r(position), theta(velocity)')
+    plt.ylabel('z')
+    #ax1.set_title('velocity field r-z direction (average over theta)')
+    Q = ax1.quiver(x_array, y_array, vx_array, vy_array,
+                   units='width',angles='xy', scale_units='xy', scale=quiver_scale,
+                   )
+    ax1.quiverkey(Q, 0.2, 0.9, label_scale,
+                  label = "labescale" + str(label_scale) + "quiver_scale" + str(quiver_scale) + '*arrow length of wall velocity in 45 degree, velocity field r-z direction',
+                  labelpos='E', coordinates='figure', angle=45)
+
+    fig1.savefig(dp.f_momentum_mass_field_rtheta_path + str(step))
+    plt.close('all')
+
+
+    quiver_scale = 0.2
+    label_scale = 0.6
+    vx_array = 0*df['c_m1'].values
+    vy_array = df['c_m1'].values/float(rr.logfile['den'])/vol_in_chunks
+    fig1, ax1 = plt.subplots()
+    fig1.set_size_inches(12.8, 9.6)
+    #fig1.figsize = [12.8, 9.6]
+    plt.xlabel('r')
+    plt.ylabel('z')
+    #ax1.set_title('velocity field r-z direction (average over theta)')
+    Q = ax1.quiver(x_array, y_array, vx_array, vy_array,
+                   units='width',angles='xy', scale_units='xy', scale=quiver_scale,
+                   )
+
+    ax1.quiverkey(Q, 0.2, 0.9,
+                  label_scale, label = "labescale" + str(label_scale) + "quiver_scale" + str(quiver_scale) + 'solid fraction',
+                  labelpos='E',
+                  coordinates='figure', angle=90)
 
     fig1.savefig(dp.f_momentum_mass_field_density_path + str(step))
     plt.close('all')
