f30d71e0eb5bf64268bd6459a65391ac5f0b65f5
ic6413 <ic6413@gmail.com>
2020-05-08 13:13:23 -0700

    nothing

--- lammps_process/tools/set_environment/set_en_run_lmp_cpu.sh
+++ lammps_process/tools/set_environment/set_en_run_lmp_cpu.sh
@@ -37,73 +37,33 @@ LMP_CMD_kno="-k off"
 LMP_CMD_kkomp="-k on t ${SLURM_CPUS_PER_TASK} -sf kk"
 LMP_OMP="-pk omp ${SLURM_CPUS_PER_TASK} -sf omp"
 
-#=======Script========================
-LMP_INSCRIPT_sm="/home/hllin/simulation/lammps_process/tools/lmp_input/in.lmpscript_simple_20190114_v11"
-LMP_INSCRIPT_kkbench_kokkos="/home/hllin/simulation/lammps_process/tools/lmp_input/kokkos_bench/in.chute_kokkos"
-LMP_INSCRIPT_kkbench_kokkos_print1="/home/hllin/simulation/lammps_process/tools/lmp_input/kokkos_bench/in.chute_kokkos_print1atom"
-LMP_INSCRIPT_ch="in.chute_print1atom"
-LMP_INSCRIPT_lj="in.lj_print1atom"
-LMP_INSCRIPT_mycy="/home/hllin/simulation/lammps_process/tools/lmp_input/in.lmpscript_cylinder"
-LMP_INSCRIPT_infolder="./in.override"
 
-LMP_INSCRIPT=${LMP_INSCRIPT_infolder}
+LMP_INSCRIPT=./in.override
 
 
+##===========normal do not bind socket============
+#LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load openmpi/4.0.1
+module load lmp/20200402_stable3Mar2020_openmpi401_gran_mpiio_useromp #190809unstable_openmpi401_mpiio #20200402_stable3Mar2020_openmpi401_gran_mpiio_useromp # 190809unstable_openmpi401_mpiio  190916master_openmpi401_omp_mpiio  #190809unstable_openmpi401_mpiio
+#check setting after load module
+module list
+${OMPIRUN_basic} lmp -in ${LMP_INSCRIPT}
+${OMPIRUN_basic} ${OMPIRUNBIND_c} lmp -in ${LMP_INSCRIPT}
+${OMPIRUN_basic} ${OMPIRUNBIND_s} lmp -in ${LMP_INSCRIPT}
 
-##### Functions
-single_script () {
-    
-    ##===========normal do not bind socket============
-    #LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-    module purge
-    module load openmpi/4.0.1
-    module load lmp/20200402_stable3Mar2020_openmpi401_gran_mpiio_useromp #190809unstable_openmpi401_mpiio #20200402_stable3Mar2020_openmpi401_gran_mpiio_useromp # 190809unstable_openmpi401_mpiio  190916master_openmpi401_omp_mpiio  #190809unstable_openmpi401_mpiio
-    #check setting after load module
-    module list
-    #${OMPIRUN_basic} lmp -in ${LMP_INSCRIPT}
-    ${OMPIRUN_basic} ${OMPIRUNBIND_c} lmp -in ${LMP_INSCRIPT}
+:'
+##===========normal do not bind socket============
+#LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module purge
+module load ompi/4.0.1_yesucx_computenode
+module load lmp/190729_master_selfompi401ucx_normal_mpiio 
+#check setting after load module
+module list
+${OMPIRUN_basic} lmp -in ${LMP_INSCRIPT}
+${OMPIRUN_basic} ${OMPIRUNBIND_c} lmp -in ${LMP_INSCRIPT}
+'
 
-    :'
-    ##===========normal do not bind socket============
-    #LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-    module purge
-    module load ompi/4.0.1_yesucx_computenode
-    module load lmp/190729_master_selfompi401ucx_normal_mpiio 
-    #check setting after load module
-    module list
-    ${OMPIRUN_basic} lmp -in ${LMP_INSCRIPT}
-    ${OMPIRUN_basic} ${OMPIRUNBIND_c} lmp -in ${LMP_INSCRIPT}
-    '
-
-    echo "All Done!"
-}
-
-###### command line option
-if_multi=0
-while [ "$1" != "" ]; do
-    case $1 in
-        --multi )               if_multi=1
-                                ;;
-        * )                     usage
-                                exit 1
-    esac
-    shift
-done
-
-# if multi
-if [ "$if_multi" == "0" ]; then
-    echo "multi is off"
-    single_script
-
-else
-    echo "multi on"
-    FILES=./*/
-    for f in ${FILES}
-    do
-        cd ${f}
-        single_script
-        cd ..
-    done
-fi
+echo "All Done!"
 
 
